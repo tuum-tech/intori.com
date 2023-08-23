8ab3fcd1ab66531e8cac773c2bdbbdb3ee954fa9
Kiran Pachhai <kiran.pachhai@gmail.com>
2023-08-23 16:55:49 -0400

    Handle invalid file format or empty items in the file

--- src/components/FileUpload.svelte
+++ src/components/FileUpload.svelte
@@ -3,6 +3,9 @@
   import { navigate } from "svelte-routing";
   import { orders, type Order } from "../utils/stores";
 
+  let showToast = false;
+  let toastMessage = "";
+
   function handleFileChange(event: Event) {
     const fileInput = event.target as HTMLInputElement;
     const file = fileInput?.files?.[0];
@@ -19,6 +22,11 @@
           }
           // Directly use the parsed data without remapping
           const parsedData = results.data.slice(1) as Order[];
+          if (parsedData.length === 0 || !parsedData[0]["Order ID"]) {
+            showToast = true;
+            toastMessage = "Invalid file format or no valid items found.";
+            return;
+          }
           orders.set(parsedData);
           navigate("/selectOrders"); // Navigate to SelectOrders page
         },
@@ -32,3 +40,23 @@
 <h1>Upload Amazon Purchase History</h1>
 <!-- File Upload -->
 <input type="file" on:change={handleFileChange} />
+{#if showToast}
+  <div class="toast">{toastMessage}</div>
+{/if}
+
+<style>
+  .toast {
+    position: fixed;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: #333;
+    color: #fff;
+    padding: 10px 20px;
+    border-radius: 5px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+    opacity: 0.9;
+    transition: opacity 0.3s;
+    z-index: 1000;
+  }
+</style>
